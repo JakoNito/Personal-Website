011e6fbc4b65b97babdcaab5eff7f7f1645f4e63
jakonito10 <jankarlorn@gmail.com>
2025-03-25 23:38:23 +1300

    refactor (title): change title font and size

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,8 +2,14 @@ function Title() {
     return (
         <div className="h-screen flex items-center justify-center">
             <div className='flex flex-col items-center'>
-                <h1 className='font-bold text-8xl text-white'>Jan Karlo Nito</h1>
-                <h2 className='text-white mx-20 font-medium text-[16px] 
+                <h1 className='text-white font-black text-white 
+                    text-[70px] sm:text-[80px] md:text-[90px] lg:text-[120px] lg:leading-[95px] text-center'
+                >
+                    Jan Karlo Nito
+                </h1>
+
+                <h2 className='text-white mx-20 font-medium 
+                    text-[16px] 
                     sm:text-[18px] md:text-[20px] lg:text-[30px] lg:leading-[40px] lg:mt-12 text-center'
                 >
                     Software developer based in Auckland, New Zealand
