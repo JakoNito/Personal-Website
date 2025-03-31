b49b1714420e18e5f3ec7c05960447ca5a385da8
jakonito10 <jankarlorn@gmail.com>
2025-04-01 01:30:33 +1300

    Refactor sizing in profile social row

--- src/components/SocialRow.jsx
+++ src/components/SocialRow.jsx
@@ -1,17 +1,17 @@
 function SocialRow() {
     return (
-        <div className="flex justify-center gap-10 mt-4">
+        <div className="flex justify-center gap-6 mt-4">
             {/* Linkedin */}
             <a href='https://www.linkedin.com/in/jan-karlo-nito/' target="_blank" rel="noopener noreferrer">
-                <img src='Linkedin.svg' className='pointer-events-none h-12 xs:sm:h-72 md:xl:h-10' alt='Linkedin'></img>
+                <img src='Linkedin.svg' className='pointer-events-none h-7' alt='Linkedin'></img>
             </a>
             {/* GitHub */}
             <a href='https://github.com/JakoNito/' target="_blank" rel="noopener noreferrer">
-                <img src='GitHub.svg' className='pointer-events-none h-12 xs:sm:h-72 md:xl:h-10' alt='GitHub'></img>
+                <img src='GitHub.svg' className='pointer-events-none h-7' alt='GitHub'></img>
             </a>
             {/* Resume */}
             <a href='/#'>
-                <h3 className="text-white font-medium text-[16px] mb-6 sm:text-[18px] md:text-[20px] lg:text-[30px]">Resume</h3>
+                <h3 className="text-white text-[16px] mb-6 sm:text-[16px] md:text-[18px] lg:text-[20px]">Resume</h3>
             </a>
         </div>
     )
