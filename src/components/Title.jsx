0bc190e0c30ddf67ee193d123e1a23a0d64e52be
jakonito10 <jankarlorn@gmail.com>
2025-04-01 00:52:35 +1300

    Remove extra background image in title section

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,21 +1,19 @@
 function Title() {
     return (
-        <div className="bg-[url(../public/queenstown.jpg)] bg-cover">
-            <div className="h-screen flex items-center justify-center backdrop-brightness-75">
-                <div className='flex flex-col items-center'>
-                    <h1 className='text-white font-black text-white 
+        <div className="h-screen flex items-center justify-center backdrop-brightness-75">
+            <div className='flex flex-col items-center'>
+                <h1 className='text-white font-black text-white 
                     text-[70px] sm:text-[80px] md:text-[90px] lg:text-[120px] lg:leading-[95px] text-center'
-                    >
-                        Jan Karlo Nito
-                    </h1>
+                >
+                    Jan Karlo Nito
+                </h1>
 
-                    <h2 className='text-white mx-20 font-medium 
+                <h2 className='text-white mx-20 font-medium 
                     text-[16px] 
                     sm:text-[18px] md:text-[20px] lg:text-[30px] lg:leading-[40px] lg:mt-12 text-center'
-                    >
-                        Software developer based in Auckland, New Zealand
-                    </h2>
-                </div>
+                >
+                    Software developer based in Auckland, New Zealand
+                </h2>
             </div>
         </div>
     )
